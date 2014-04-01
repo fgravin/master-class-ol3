13e0d9a0ca739340af4c2c4917078f702de93637
Éric Lemoine <eric.lemoine@gmail.com>
2014-04-01 11:27:54 +0200

    Update vector-layer example

--- slides/examples/vector-layer.js
+++ slides/examples/vector-layer.js
@@ -1,25 +1,28 @@
-var raster = new ol.layer.TileLayer({
-  source: new ol.source.MapQuestOpenAerial()
+var raster = new ol.layer.Tile({
+  source: new ol.source.MapQuest({
+    layer: 'sat'
+  })
 });
 
 var vector = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    projection: ol.proj.get('EPSG:4326')
+  source: new ol.source.GeoJSON({
+    projection: 'EPSG:3857',
+    url: 'data/countries.json'
   }),
-  style: new ol.style.Style({rules: [
-    new ol.style.Rule({
-      symbolizers: [
-        new ol.style.Polygon({
-          strokeColor: '#bada55'
-        })
-      ]
+  style: new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255,255,255,0.2)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#bada55',
+      width: 2
     })
-  ]})
+  })
 });
 
 var map = new ol.Map({
   layers: [raster, vector],
-  renderer: ol.RendererHint.CANVAS,
+  renderer: 'canvas',
   target: 'map',
   view: new ol.View2D({
     center: [0, 0],
@@ -27,21 +30,53 @@ var map = new ol.Map({
   })
 });
 
+var popup = new ol.Overlay({
+  element: document.getElementById('popup')
+});
+map.addOverlay(popup);
 
-var geojson = new ol.parser.GeoJSON();
-var url = 'data/countries.json';
-var xhr = new XMLHttpRequest();
-xhr.open('GET', url, true);
-
-
-/**
- * onload handler for the XHR request.
- */
-xhr.onload = function() {
-  if (xhr.status == 200) {
-    // this is silly to have to tell the layer the destination projection
-    var projection = map.getView().getProjection();
-    vector.parseFeatures(xhr.responseText, geojson, projection);
+$(map.getViewport()).on('mousemove', function(evt) {
+  var coordinate = map.getEventCoordinate(evt.originalEvent);
+  var pixel = map.getEventPixel(evt.originalEvent);
+  var feature = map.forEachFeatureAtPixel(pixel, function(feature, layer) {
+    return feature;
+  });
+  var element = popup.getElement();
+  $(element).popover('destroy');
+  if (feature) {
+    var name = feature.get('name');
+    popup.setPosition(coordinate);
+    $(element).popover({
+      placement: 'top',
+      animation: false,
+      html: true,
+      content: name
+    });
+    $(element).popover('show');
   }
-};
-xhr.send();
+});
+
+//map.on('mousemove', function(evt) {
+  //map.getFeatureInfo({
+    //pixel: evt.getPixel(),
+    //layers: [vector],
+    //success: function(features) {
+      //var info = [];
+      //for (var i = 0, ii = features.length; i < ii; ++i) {
+        //info.push(features[i].get('i'));
+      //}
+      //var element = popup.getElement();
+      //var coordinate = evt.getCoordinate();
+      //popup.setPosition(coordinate);
+      //$(element).popover('destroy');
+      //$(element).popover({
+        //'placement': 'top',
+        //'animation': false,
+        //'html': true,
+        //'content': info.join(', ')
+      //});
+      //$(element).popover(info.length ? 'show' : 'hide');
+    //}
+  //});
+//});
+
