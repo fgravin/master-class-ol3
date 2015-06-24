ec17e33e7dc937752a50ef273a84a3fa7c8b1bef
Florent gravin <florent.gravin@camptocamp.com>
2015-06-24 13:19:09 +0200

    Updates

---
+++ examples/overlays.js
@@ -0,0 +1,40 @@
+var seattle = [-13618019.918127416, 6050307.967492877];
+
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.Stamen({
+        layer: 'watercolor'
+      })
+    })
+  ],
+  view: new ol.View({
+    center: seattle,
+    zoom: 11
+  })
+});
+
+var popup = new ol.Overlay({
+  element: document.getElementById('popup')
+});
+map.addOverlay(popup);
+
+var hdms;
+function getContent() {
+  return '<p>The location you clicked is:</p><code>' + hdms + '</code>';
+}
+
+map.on('singleclick', function(evt) {
+  var element = popup.getElement();
+  var coordinate = evt.coordinate;
+  hdms = ol.coordinate.toStringHDMS(ol.proj.transform(
+      coordinate, 'EPSG:3857', 'EPSG:4326'));
+  popup.setPosition(coordinate);
+  $(element).popover({
+    'placement': 'top',
+    'html': true,
+    'content': getContent
+  });
+  $(element).popover('show');
+});
---
+++ examples/tiles-galore.js
@@ -0,0 +1,15 @@
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.BingMaps({
+        key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3',
+        imagerySet: 'Aerial'
+      })
+    })
+  ],
+  view: new ol.View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
